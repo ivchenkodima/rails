6182a78cb28e2aec6995fcf356967c3e1d18dc28
stranger <ivchenkodima@gmail.com>
2015-05-25 17:58:29 +0300

    test

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,4 +8,7 @@ class Product < ActiveRecord::Base
 	 message: 'must be a URL for GIF, JPG or PNG image.'
 	 # URL должен указывать на изображение формата GIF, JPG или PNG
 	 }
+	 def self.latest
+ 		Product.order(:updated_at).last
+end
 end
