d5a3759cc5f52551ec114729999187bb07534ea7
stranger <ivchenkodima@gmail.com>
2015-05-28 01:46:25 +0300

    done chatper 12

--- test/controllers/orders_controller_test.rb
+++ test/controllers/orders_controller_test.rb
@@ -11,6 +11,12 @@ class OrdersControllerTest < ActionController::TestCase
     assert_not_nil assigns(:orders)
   end
 
+test "requires item in cart" do
+   get :new
+   assert_redirected_to store_path
+   assert_equal flash[:notice], 'Your cart is empty'
+end
+
   test "should get new" do
     get :new
     assert_response :success
@@ -21,7 +27,7 @@ class OrdersControllerTest < ActionController::TestCase
       post :create, order: { address: @order.address, email: @order.email, name: @order.name, pay_type: @order.pay_type }
     end
 
-    assert_redirected_to order_path(assigns(:order))
+    assert_redirected_to  store_path
   end
 
   test "should show order" do
